e928d04fc5227940cc3f8b97257d9fb46f82bb1c
Zixuan Xu <43517559+xuzixuan1998@users.noreply.github.com>
2023-06-11 12:22:04 +0200

    Update finetune.py

--- finetune.py
+++ finetune.py
@@ -155,8 +155,7 @@ def generate_saliency_map(model, ids, masks, features, tokens):
     ids_gradients_2 /= ids_gradients_2.max()
     features_gradients_1 = torch.hstack([token_gradient_1, features_gradients_1])
     features_gradients_2 = torch.hstack([token_gradient_2, features_gradients_2])
-    pdb.set_trace()
-    data[len(data)] = {'p1':tokens[0], 'p2':tokens[1], 'p1_gradients':ids_gradients_1, 'p2_gradients':ids_gradients_2, 'feature1_gradients':features_gradients_1, 'feature2_gradients':features_gradients_2}
+    data[len(data)] = {'p1':tokens[0], 'p2':tokens[1], 'p1_gradients':ids_gradients_1.numpy(), 'p2_gradients':ids_gradients_2.numpy(), 'feature1_gradients':features_gradients_1.numpy(), 'feature2_gradients':features_gradients_2.numpy()}
     with open('saliency.json', 'w') as f:
       json.dump(data, f)
 
